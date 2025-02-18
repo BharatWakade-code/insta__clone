f1d5abe50984e38484354dbb7324c6d1569680bd
Bharat Wakade <155426524+BharatWakade-code@users.noreply.github.com>
2025-02-18 19:27:36 +0530

    add shimer effect

--- lib/Screens/home/posts/components/post_card.dart
+++ lib/Screens/home/posts/components/post_card.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:insta_clone/Components/photoview.dart';
 import 'package:insta_clone/Screens/home/home_cubit.dart';
 import 'package:insta_clone/Screens/home/posts/components/like_animation.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 class PostCard extends StatefulWidget {
@@ -36,7 +37,6 @@ class _PostCardState extends State<PostCard> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Image Section with Cover Image
               Stack(
                 alignment: Alignment.center,
                 children: [
@@ -56,40 +56,20 @@ class _PostCardState extends State<PostCard> {
                         ),
                       );
                     },
-                    child: Container(
-                      constraints:
-                          const BoxConstraints(minHeight: 400, maxHeight: 1000),
-                      width: double.infinity,
-                      child: CachedNetworkImage(
-                        fadeInCurve: Easing.standardAccelerate,
-                        fadeInDuration: Durations.long1,
-                        imageUrl: snap['posturl'],
-                        fit: BoxFit.cover,
-                        progressIndicatorBuilder:
-                            (context, url, downloadProgress) {
-                          return Container(
-                            constraints: const BoxConstraints(
-                                minHeight: 400, maxHeight: 500),
-                            width: double.infinity,
-                            child: Center(
-                              child: CircularProgressIndicator(
-                                value: downloadProgress.progress,
-                                color: Colors.black,
-                              ),
-                            ),
-                          );
-                        },
-                        errorWidget: (context, url, error) {
-                          return Container(
-                            constraints: const BoxConstraints(
-                                minHeight: 400, maxHeight: 500),
-                            width: double.infinity,
-                            child: const Center(
-                              child: Icon(Icons.error, color: Colors.red),
-                            ),
-                          );
-                        },
+                    child: CachedNetworkImage(
+                      imageUrl: snap['posturl'],
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) => Shimmer.fromColors(
+                        baseColor: Colors.grey[300]!,
+                        highlightColor: Colors.grey[100]!,
+                        child: Container(
+                          height: 400,
+                          width: double.infinity,
+                          color: Colors.white,
+                        ),
                       ),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error, color: Colors.red),
                     ),
                   ),
                   AnimatedOpacity(
@@ -97,14 +77,8 @@ class _PostCardState extends State<PostCard> {
                     opacity: cubit.isLikeAnimating ? 1 : 0,
                     child: LikeAnimation(
                       isAnimating: cubit.isLikeAnimating,
-                      duration: const Duration(
-                        milliseconds: 400,
-                      ),
-                      onEnd: () {
-                        setState(() {
-                          cubit.isLikeAnimating = false;
-                        });
-                      },
+                      duration: const Duration(milliseconds: 400),
+                      onEnd: () => cubit.isLikeAnimating = false,
                       child: const Icon(
                         Icons.favorite,
                         color: Colors.white,
@@ -114,130 +88,85 @@ class _PostCardState extends State<PostCard> {
                   ),
                 ],
               ),
-              // User Information Section
               Padding(
                 padding: const EdgeInsets.all(12),
                 child: Row(
                   children: [
-                    // Profile Image
                     ClipOval(
                       child: CachedNetworkImage(
                         imageUrl: snap['profimage'],
                         height: 40,
                         width: 40,
                         fit: BoxFit.cover,
+                        placeholder: (context, url) => Shimmer.fromColors(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor: Colors.grey[100]!,
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error, color: Colors.red),
                       ),
                     ),
                     const SizedBox(width: 12),
-
-                    // Username, Location, and Time
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Row(
-                          children: [
-                            Text(
-                              snap['username'],
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            const SizedBox(width: 4),
-
-                            // Caption Section
-                            if (snap['description'] != null &&
-                                snap['description'].toString().isNotEmpty)
-                              Text(
-                                snap['description'],
-                                style: TextStyle(
-                                  fontSize: 8,
-                                  color: Colors.grey[600],
-                                  fontWeight: FontWeight.w500,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                          ],
+                        Text(
+                          snap['username'],
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                         if (snap['location'] != null &&
-                            snap['location'].toString().isNotEmpty)
+                            snap['location'].isNotEmpty)
                           Text(
-                            snap['location'] ?? '',
+                            snap['location'],
                             style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey[600],
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
+                                fontSize: 12, color: Colors.grey[600]),
                           ),
                         Text(
                           timeago.format(
-                            DateTime.parse(snap['datepublished']).toLocal(),
-                          ),
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: Colors.grey[500],
-                          ),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
+                              DateTime.parse(snap['datepublished']).toLocal()),
+                          style:
+                              TextStyle(fontSize: 10, color: Colors.grey[500]),
                         ),
                       ],
                     ),
                     const Spacer(),
-
-                    // Icons (Like and Bookmark)
                     Row(
                       children: [
                         LikeAnimation(
-                          isAnimating: widget.snap['likes'].contains(
-                            cubit.currentUserUid,
-                          ),
+                          isAnimating:
+                              snap['likes'].contains(cubit.currentUserUid),
                           smallLike: true,
                           child: IconButton(
-                            icon: widget.snap['likes'].contains(
-                              cubit.currentUserUid,
-                            )
-                                ? const Icon(
-                                    Icons.favorite,
-                                    color: Colors.red,
-                                  )
-                                : const Icon(
-                                    Icons.favorite_border,
-                                  ),
-                            onPressed: () async {
-                              cubit.likePost(snap['postid'], snap['likes']);
-                            },
+                            icon: snap['likes'].contains(cubit.currentUserUid)
+                                ? const Icon(Icons.favorite, color: Colors.red)
+                                : const Icon(Icons.favorite_border),
+                            onPressed: () =>
+                                cubit.likePost(snap['postid'], snap['likes']),
                           ),
                         ),
-                        Text(
-                          '${widget.snap['likes'].length} likes',
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: Colors.grey[500],
-                          ),
-                        ),
+                        Text('${snap['likes'].length} likes',
+                            style: TextStyle(
+                                fontSize: 10, color: Colors.grey[500])),
                         IconButton(
-                          icon: const Icon(
-                            Icons.chat_bubble_outline_rounded,
-                            color: Colors.black,
-                            size: 22,
-                          ),
+                          icon: const Icon(Icons.chat_bubble_outline_rounded),
                           onPressed: () {
-                            // Handle comment action
+
                           },
                         ),
                         IconButton(
-                          icon: const Icon(
-                            Icons.bookmark_border,
-                            color: Colors.black,
-                            size: 22,
-                          ),
-                          onPressed: () {
-                            // Handle bookmark action
-                          },
+                          icon: const Icon(Icons.bookmark_border),
+                          onPressed: () {},
                         ),
                       ],
                     ),
